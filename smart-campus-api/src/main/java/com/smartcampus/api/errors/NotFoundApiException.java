5cafdd29c701d82188421680d7495f3b0fde680a
induwara-cmd <indendz0118@gmail.com>
2026-04-13 07:51:44 +0530

    .

---
+++ smart-campus-api/src/main/java/com/smartcampus/api/errors/NotFoundApiException.java
@@ -0,0 +1,8 @@
+package com.smartcampus.api.errors;
+
+public final class NotFoundApiException extends ApiException {
+  public NotFoundApiException(String safeMessage) {
+    super(404, safeMessage);
+  }
+}
+
---
+++ smart-campus-api/src/main/java/com/smartcampus/api/errors/UnprocessableEntityException.java
@@ -0,0 +1,8 @@
+package com.smartcampus.api.errors;
+
+public final class UnprocessableEntityException extends ApiException {
+  public UnprocessableEntityException(String safeMessage) {
+    super(422, safeMessage);
+  }
+}
+
